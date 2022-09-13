ea7afa2025349d4a0b3c67cb2553919cd90b277a
Jaum_https <joao.vitor.pires.060@gmail.com>
2022-09-13 11:45:08 -0300

    Error tsconfig.json

--- src/server.mjs
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-const app = express()
-
-app.get ('/router', (req, res) => {
-  res.send('Router test')
-})
-
-app.listen(8080, ()=> console.log('Lintening in port 8080'))
---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.get ('/router', (req, res) => {
+  return res.status(200).json({
+    name: "test name",
+    age: 12
+  })
+})
+
+app.listen(8080, () => console.log('Lintening in port 8080'))
